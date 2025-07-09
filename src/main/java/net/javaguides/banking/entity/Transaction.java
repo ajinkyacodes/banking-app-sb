f186f226048cfd3950d39de54df740632a92c6c2
AJINKYA <ajinkyacodes@gmail.com>
2025-07-09 21:42:41 +0530

    created transaction entity for creating new table in database called transactions

---
+++ src/main/java/net/javaguides/banking/entity/Transaction.java
@@ -0,0 +1,26 @@
+package net.javaguides.banking.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long accountId;
+    private double amount;
+    private String transactiontype; // DEPOSIT, WITHDRAW, TRANSFER
+    private LocalDateTime timestamp;
+}
