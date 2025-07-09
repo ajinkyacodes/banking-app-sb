5fa750717077cfc9df0cb5ec74f705680fb66de9
AJINKYA <ajinkyacodes@gmail.com>
2025-07-09 17:56:08 +0530

    Deposit Amount REST API created

--- src/main/java/net/javaguides/banking/service/impl/AccountServiceImpl.java
+++ src/main/java/net/javaguides/banking/service/impl/AccountServiceImpl.java
@@ -28,4 +28,13 @@ public class AccountServiceImpl implements AccountService {
         Account account = accountRepository.findById(id).orElseThrow(() -> new RuntimeException("Account does not exists."));
         return AccountMapper.mapToAccountDto(account);
     }
+
+    @Override
+    public AccountDto deposite(Long id, double amount) {
+        Account account = accountRepository.findById(id).orElseThrow(() -> new RuntimeException("Account does not exists."));
+        double total = account.getBalance() + amount;
+        account.setBalance(total);
+        Account savedAccount = accountRepository.save(account);
+        return AccountMapper.mapToAccountDto(savedAccount);
+    }
 }
