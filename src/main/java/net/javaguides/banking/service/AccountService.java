0fe0abc4529a88f948e2fd97353228cc12d7c16c
AJINKYA <ajinkyacodes@gmail.com>
2025-07-09 18:19:56 +0530

    get all accounts REST API created

--- src/main/java/net/javaguides/banking/service/AccountService.java
+++ src/main/java/net/javaguides/banking/service/AccountService.java
@@ -2,6 +2,8 @@ package net.javaguides.banking.service;
 
 import net.javaguides.banking.dto.AccountDto;
 
+import java.util.List;
+
 public interface AccountService {
     AccountDto createAccount(AccountDto accountDto);
 
@@ -10,4 +12,6 @@ public interface AccountService {
     AccountDto deposite(Long id, double amount);
 
     AccountDto withdraw(Long id, double amount);
+
+    List<AccountDto> getAllAccounts();
 }
